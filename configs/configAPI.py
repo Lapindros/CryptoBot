da9c37f9f658a8cffa207edb6baf3fc9d768c6d8
Lapindros <31004280+Lapindros@users.noreply.github.com>
2024-02-27 13:34:27 +0400

    Update configAPI.py

--- configs/configAPI.py
+++ configs/configAPI.py
@@ -1,8 +1,8 @@
 CONFIG_API = {
-    'BOT_TOKEN': '5446254348:AAHpbZhWHaTUWxVFpqG1yhe4Hra2M51vrEQ',
-    'ADMIN_ID': 471969761,
-    'BINANCE_API_KEY': '9jzp28gVnOhyvwC30bKJyQJAXuiUhV5YsnfBcjlPVfZAR9tFKiBsDhVXetadYHOB',
-    'BINANCE_API_SECRET': 'Hk20Sc68nScx4H36Br07XYE7LY8LpG5nd648MWn7wfBir3JL9wgTPnbfrudeXesS',
+    'BOT_TOKEN': '<telegram_token>',
+    'ADMIN_ID': <admin_telegram_token>,
+    'BINANCE_API_KEY': '<BINANCE_API_KEY>',
+    'BINANCE_API_SECRET': '<BINANCE_API_SECRET>',
 
     'BINANCE_LIVE_HOST': 'https://api1.binance.com/api',
 
